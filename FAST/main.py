564590aaf21142f7dc81af1eb15f1d8abf116f1b
MarianoOrduna <122043964@upq.edu.mx>
2025-02-11 20:25:22 -0600

    Practica diccionario HTTP
    Creamos endpoints para actualizar, agregar y consultar usuarios

--- FAST/main.py
+++ FAST/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from typing import Optional
 
 app = FastAPI(
     title = 'Mi PrimerAPI 192',
-    description = 'Bernal',
+    description = 'Mariano',
     version = '1.0.1'
 )
 
@@ -15,37 +15,70 @@ usuarios = [
 ]
 
 
-# Endpoint home
+
+#Endpoint home
 @app.get('/', tags=['Hola Mundo'])
 def home():
     return {'hello': 'world FastAPI'}
 
+
+#endpoint Consulta todos
+@app.get('/todosUsuarios', tags=['Operaciones CRUD'])
+def leerUsuarios():
+    return{'Los usuarios registrados son': usuarios}
+
+#endpoint agregar nuevos
+@app.post('/usuario/', tags=['Operaciones CRUD'])
+def agregarUsuario(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
+            raise HTTPException(status_code=400, detail="ID ya existente")
+    usuarios.append(usuario)
+    return usuario
+
+#enpoint actualizar usuarios
+@app.put('/usuarios/{id}',tags=['Operaciones CRUD'])
+def actualizar(id:int,usuarioActualizado:dict):
+    for index, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            usuarios[index].update(usuarioActualizado)
+            return usuarios[index]
+    raise HTTPException(status_code=400, detail="El usuario no existe")
+
+
+#aqui es importante agregar parametros
+#En los parametros va primero el nombre y despues el tipo de dato
+
+
+
 #endpoint promedio
-@app.get('/promedio', tags=['Mi Calificacion Parcial'])
-def promedio():
-    return 10
+#@app.get('/promedio', tags=['Mi Calificacion Parcial'])
+#def promedio():
+    #return 10
     
 #endpoint parametros
-@app.get('/usuario/{id}', tags=['Parametro Obligatorio'])
-def ConsultaUsuario(id:int):
+#@app.get('/usuario/{id}', tags=['Parametro Obligatorio'])
+#def ConsultaUsuario(id:int):
     #conectamos a la Bd
     #consultamos
-    return {'Se encontro el Usuario':id}
+ #   return {'Se encontro el Usuario':id}
 
 # Endpoint parámetro opcional
-@app.get("/usuario/", tags=["Parametro Opcional"])
-def consultausuario(id: Optional[int] = None):
-    if id is not None:
-        for usu in usuarios:
-            if usu["id"] == id:
-                return {"mensaje": "Usuario encontrado", "usuario": usu}
+#@app.get("/usuario/", tags=["Parametro Opcional"])
+#def consultausuario(id: Optional[int] = None):
+ #   if id is not None:
+  #      for usu in usuarios:
+   #         if usu["id"] == id:
+    #            return {"mensaje": "Usuario encontrado", "usuario": usu}
         
-        return {"mensaje": f"No se encontró el usuario con id: {id}"}
-    else :
-        return {"mensaje": "No se proporcionó un id"}
+     #   return {"mensaje": f"No se encontró el usuario con id: {id}"}
+    #lse :
+     #   return {"mensaje": "No se proporcionó un id"}
 
 
 #cd fast
 #Get-ExecutionPolicy 
 #.\entornofast\Scripts\activate
 #uvicorn main:app --reload --port 5000
+#Get-ExecutionPolicy
+#Set-ExecutionPolicy Unrestricted -Scope Process
