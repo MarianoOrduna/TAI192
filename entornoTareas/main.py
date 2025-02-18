42f2dff756806f24423591ea50323afc54137c54
MarianoOrduna <122043964@upq.edu.mx>
2025-02-18 08:11:12 -0600

    Se realizo el endpoint para actualizar tareas
    Se hizo el endpoint para actualizar una tarea existente

--- entornoTareas/main.py
+++ entornoTareas/main.py
@@ -10,27 +10,27 @@ app = FastAPI(
 tareas=[
     {"id": 1,
     "Titulo": "Estudiar para el examen",
-    "Descripcion": 'Repasar los apuntes de TAI',
-    'Vencimiento':'14-02-25',
-    'Estado':'Completada'},
+    "Descripcion": "Repasar los apuntes de TAI",
+    "Vencimiento":"14-02-25",
+    "Estado":"Completada"},
 
     {"id": 2,
     "Titulo": "Practica fastAPI",
-    "Descripcion": 'Hacer el reporte de fastAPI',
-    'Vencimiento':'28-02-25',
-    'Estado':'Pendiente'},
+    "Descripcion": "Hacer el reporte de fastAPI",
+    "Vencimiento":"28-02-25",
+    "Estado":"Pendiente"},
 
     {"id": 3,
     "Titulo": "Exposicion de redes",
-    "Descripcion": 'Realizar una presentacion acerca de como se crearon las redes',
-    'Vencimiento':'30-03-25',
-    'Estado':'Pendiente'},
+    "Descripcion": "Realizar una presentacion acerca de como se crearon las redes",
+    "Vencimiento":"30-03-25",
+    "Estado":"Pendiente"},
 
     {"id": 4,
     "Titulo": "Investigacion fastAPI",
     "Descripcion": 'Realizar una investigacion acerca de fastAPI',
-    'Vencimiento':'5-02-25',
-    'Estado':'No realizada'}
+    "Vencimiento":"5-02-25",
+    "Estado":"No realizada"}
 ]
 
 #Endpoint home
@@ -56,3 +56,14 @@ def agregarTarea(tarea:dict):
             raise HTTPException(status_code=400, detail='ID ya existente')
     tareas.append(tarea)   
     return tarea
+
+#Endpoint para actualizar una tarea existente
+@app.put('/usuarios/{id}',tags=['Operaciones CRUD'])
+def actualizar(id:int,tareaActualizada:dict):
+    for index, usr in enumerate(tareas):
+        if usr['id'] == id:
+            tareas[index].update(tareaActualizada)
+            return tareas[index]
+    raise HTTPException(status_code=400, detail='La tarea no existe')
+
+
